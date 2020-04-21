67fac72237b8b0e097311e1f4679d4fe08ab45fc
Vasil Bachev <vasil@vasilbachev.com>
2020-04-21 22:55:06 +0300

    Updates

---
+++ precache-manifest.b93e4b3c4a33e92945687679f64e3e6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0c8a7bc2a52b407c2fac8ab5bc84b0",
+    "url": "/domus/index.html"
+  },
+  {
+    "revision": "c441eeceab0156d7a5f0",
+    "url": "/domus/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c7153fc04018b3bee1f8",
+    "url": "/domus/static/css/main.507877fb.chunk.css"
+  },
+  {
+    "revision": "c441eeceab0156d7a5f0",
+    "url": "/domus/static/js/2.be407806.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/domus/static/js/2.be407806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7153fc04018b3bee1f8",
+    "url": "/domus/static/js/main.6723971d.chunk.js"
+  },
+  {
+    "revision": "47d60020baf481a55660",
+    "url": "/domus/static/js/runtime-main.f4a8f33d.js"
+  }
+]);
