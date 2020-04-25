36d4c8cac957bb4c185646805580093065fccbf8
Vasil Bachev <vasil@vasilbachev.com>
2020-04-26 02:24:05 +0300

    Move classes to dataset.ts

--- src/dataset.ts
+++ src/dataset.ts
@@ -1,7 +1,80 @@
 import { SpreadsheetData, getCachedData, fetchAllData, insertRow } from './store/googleSheets'
-import { Transaction, Account, Entity, Note } from './types'
 
-export interface Content {
+class Transaction {
+	[key: string]: any
+	date: Date
+	formattedDate: string
+	account: string
+	amount: number
+	person: string
+	entity: string
+	comment: string
+	// @TODO: hash an id for use in loop keys
+
+	constructor(row: string[]) {
+		const [day, month, year] = row[0].split('.').map(Number);
+		this.date = new Date(year, month - 1, day) // months are 0-based
+		this.formattedDate = row[0]
+		this.account = row[3]
+		this.amount = Number(row[4])
+		this.person = row[2]
+		this.entity = row[1]
+		this.comment = row[5]
+	}
+}
+
+class Entity {
+	name: string
+	transactions: Transaction[]
+	people: string[]
+
+	constructor(transactions: Transaction[]) {
+		this.name = transactions[0].entity;
+		this.transactions = transactions;
+		this.people = Array.from(new Set(transactions.map(t => t.person)));
+	}
+
+	getLastPerson(): string {
+		return this.transactions[0].person;
+	}
+
+	getMonthsOverdue(): number {
+		const lastTransaction = this.transactions[0];
+		const now = new Date();
+		const yearsAgo = now.getFullYear() - lastTransaction.date.getFullYear();
+		const monthsAgo = now.getMonth() - lastTransaction.date.getMonth();
+		return yearsAgo * 12 + monthsAgo;
+	}
+}
+
+class Account {
+	name: string
+	transactions: Transaction[]
+	amount: number
+
+	constructor(transactions: Transaction[]) {
+		this.name = transactions[0].account;
+		this.transactions = transactions;
+		this.amount = transactions.reduce((sum, t) => sum + t.amount, 0);
+	}
+}
+
+class Note {
+	date: Date
+	formattedDate: string
+	about: string
+	message: string
+
+	constructor(note: string[]) {
+		const [day, month, year] = note[0].split('.').map(Number);
+		this.date = new Date(year, month - 1, day) // months are 0-based
+		this.formattedDate = note[0]
+		this.about = note[1]
+		this.message = note[2]
+	}
+}
+
+interface Content {
 	transactions: Transaction[]
 	accounts: Account[]
 	entities: Entity[]
