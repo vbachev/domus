e50aea336e45c0cd252e5c485f64ec5d73153196
Vasil Bachev <vasil@vasilbachev.com>
2020-05-16 16:27:19 +0300

    Fix github pages 404

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from "react-router-dom"
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom"
 import { name as appName } from '../../package.json'
 import Navigation from './Navigation'
 import HomePage from './HomePage'
@@ -8,20 +8,29 @@ import EntityPage from './EntityPage'
 import ScrollToTop from './ScrollToTop'
 import StoreProvider from './StoreProvider'
 
+const searchParams = new URLSearchParams(window.location.search)
+const redirectTo = searchParams.get('redirectTo')
+
 export default function App() {
 	return (
 		<BrowserRouter basename={appName}>
-			<StoreProvider>
-				<ScrollToTop />
-				<Navigation />
-				<div className='container py-3'>
-					<Switch>
-						<Route exact path='/' component={HomePage} />
-						<Route exact path='/entities' component={EntitiesPage} />
-						<Route exact path='/entity/:name' component={EntityPage} />
-					</Switch>
-				</div>
-			</StoreProvider>
+			{redirectTo
+				? (
+					<Redirect to={redirectTo.split(appName).pop() as string} />
+				)
+				: (
+					<StoreProvider>
+						<ScrollToTop />
+						<Navigation />
+						<div className='container py-3'>
+							<Switch>
+								<Route exact path='/' component={HomePage} />
+								<Route exact path='/entities' component={EntitiesPage} />
+								<Route exact path='/entity/:name' component={EntityPage} />
+							</Switch>
+						</div>
+					</StoreProvider>
+				)}
 		</BrowserRouter>
 	);
 }
